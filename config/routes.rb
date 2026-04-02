d78e4055a6077562365066a99f331ab4c2b17fef
jigangdade60 <255278098+jigangdade60@users.noreply.github.com>
2026-04-02 08:37:17 +0000

    レイアウト修正により各画面が正しく遷移するよう対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-scope module: :public do
-  root "homes#top"
-  get "about", to: "homes#about"
+Rails.application.routes.draw do
+  scope module: :public do
+    root "homes#top"
+    get "about", to: "homes#about"
 
-  get "sign_up", to: "users#new"
-  post "sign_up", to: "users#create"
+    get "sign_up", to: "users#new"
+    post "sign_up", to: "users#create"
+  end
+
+  resource :session, only: [:new, :create, :destroy]
 end
