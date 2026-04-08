90b282a51a46c13eed93630804f438018920df4b
jigangdade60 <255278098+jigangdade60@users.noreply.github.com>
2026-04-08 07:22:36 +0000

    学習時間が0分になる不具合を修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  # 仮想属性（フォーム用）
   attr_accessor :study_time_hour, :study_time_minute
 
-  # バリデーション
   validates :title, presence: true
   validates :body, presence: true
   validates :study_time,
@@ -13,10 +11,8 @@ class Post < ApplicationRecord
 
   validate :study_time_minute_range
 
-  # 保存前に時間＋分 → 分へ変換
   before_validation :combine_study_time
 
-  # 編集画面用（分 → 時間・分）
   def study_time_hour
     return 0 if study_time.blank?
     study_time / 60
@@ -30,6 +26,8 @@ class Post < ApplicationRecord
   private
 
   def combine_study_time
+    return if @study_time_hour.blank? && @study_time_minute.blank?
+
     hour = @study_time_hour.to_i
     minute = @study_time_minute.to_i
 
