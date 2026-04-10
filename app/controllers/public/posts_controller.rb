fa6eec4ec92f206bd48c47618b291d0cbdebe40a
jigangdade60 <255278098+jigangdade60@users.noreply.github.com>
2026-04-10 04:15:44 +0000

    いいね機能を非同期通信に対応

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,36 +1,27 @@
 class Public::PostsController < ApplicationController
-  # 一覧・詳細はログイン不要
   allow_unauthenticated_access only: %i[index show]
 
-  # 投稿取得
   before_action :set_post, only: %i[show edit update destroy]
-
-  # 投稿者本人チェック
   before_action :ensure_correct_user, only: %i[edit update destroy]
 
-  # 投稿一覧
   def index
     @tags = Tag.order(:name)
 
-    @posts = Post.includes(:user, :tags)
+    @posts = Post.includes(:user, :tags, :likes, :comments)
                  .order(created_at: :desc)
                  .keyword_search(params[:keyword])
                  .tag_search(params[:tag_name])
   end
 
-  # 投稿詳細
   def show
-    @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments.includes(:user).order(created_at: :desc)
   end
 
-  # 新規投稿
   def new
     @post = Post.new
   end
 
-  # 作成処理
   def create
     @post = Current.user.posts.build(post_params)
 
@@ -43,11 +34,6 @@ class Public::PostsController < ApplicationController
     end
   end
 
-  # 編集画面
-  def edit
-  end
-
-  # 更新処理
   def update
     if @post.update(post_params)
       @post.save_tags(post_params[:tag_names])
@@ -58,7 +44,6 @@ class Public::PostsController < ApplicationController
     end
   end
 
-  # 削除処理
   def destroy
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しました。"
@@ -66,17 +51,14 @@ class Public::PostsController < ApplicationController
 
   private
 
-  # 投稿取得
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # パラメータ
   def post_params
     params.require(:post).permit(:title, :body, :study_time_hour, :study_time_minute, :tag_names)
   end
 
-  # 本人チェック
   def ensure_correct_user
     return if @post.user == Current.user
 
