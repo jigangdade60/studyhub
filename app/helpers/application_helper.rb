bb678b36ce80f8489267cc7996981a865d024c92
jigangdade60 <255278098+jigangdade60@users.noreply.github.com>
2026-04-13 12:38:42 +0000

    ヘッダーと投稿画面にユーザーアイコン表示を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,19 @@ module ApplicationHelper
       "#{remaining_minutes}分"
     end
   end
+
+  def user_avatar(user, size: 40, extra_class: "")
+    image =
+      if user&.profile_image&.attached?
+        user.profile_image
+      else
+        "no_image.jpg"
+      end
+
+    image_tag(
+      image,
+      size: "#{size}x#{size}",
+      class: "avatar-icon rounded-circle #{extra_class}"
+    )
+  end
 end
