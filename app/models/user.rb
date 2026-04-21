8aebe80e55e2ce9470edc3f128fc26bd55b6f120
jigangdade60 <255278098+jigangdade60@users.noreply.github.com>
2026-04-21 11:09:56 +0000

    いいね機能といいね一覧表示機能にコメントを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  
+  # ユーザーがいいねした投稿を中間テーブル経由で取得できるようにする
   has_many :liked_posts, through: :likes, source: :post
 
   # =========================
